b5462ce2aa871bee20c87f06f183bae5b85bb5e5
JoshikaSree <Joshikasreepolaku@gmail.com>
2025-11-29 13:44:36 +0530

    add project

--- quiz-service/src/main/java/com/quizservice/repository/QuizRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quizservice.repository;
-
-import com.quizservice.model.Quiz;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface QuizRepository extends MongoRepository<Quiz, String> {
-}
